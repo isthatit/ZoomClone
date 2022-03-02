d40c57a80bb56cc5ab980a4c21b49b8bb192d751
isthatit <isthatitdev@gmail.com>
2022-03-03 01:37:15 +0900

    ws 설정, server/client 간 ws 연결확인

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Noom
 
 Zoom Clone using NodeJS, WebRTC and Websockets
+
+https://nomadcoders.co/noom
--- src/server.js
+++ src/server.js
@@ -1,12 +1,34 @@
 import express from "express";
+import WebSocket from "ws";
+import http from "http";
+
+import { redirect } from "express/lib/response";
 
 const app = express();
 
 app.set("view engine" ,"pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
-app.get("/", (req, res) => res.render("home"));
+const handleListen = () => console.log(`Listening on ws||http://localhost:3000`)
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`)
-app.listen(3000, handleListen);
+// http server
+const server = http.createServer(app);
+
+// websocket server
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser");
+    socket.on("close", () => {
+        console.log("Disconnected from the Browser");
+    })
+    
+    socket.on("message", (message) => {
+        console.log(message.toString('utf-8'));
+    })
+});
+
+server.listen(3000, handleListen);
